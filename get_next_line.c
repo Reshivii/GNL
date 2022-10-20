0d30e7d025162be6dd522ec1995e4e6104292f42
aburnott <aburnott@student.s19.be>
2022-10-20 12:52:39 +0200

    just main

--- get_next_line.c
+++ get_next_line.c
@@ -3,11 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aburnott <aburnott@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:12:28 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/19 15:12:39 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/20 12:26:23 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+
+char	*get_next_line(int fd)
+{
+	
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test", O_RDONLY);
+	while (1)
+	{
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free(line);
+	}
+
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aburnott <aburnott@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:08:01 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/19 15:13:17 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/20 12:24:42 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-
+# include <fcntl.h>
 
 #endif
